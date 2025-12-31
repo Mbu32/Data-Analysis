55fb1004da6aba58ec5a4982fe6c12b222c65134
Marwan <marwan.burhani@hotmail.com>
2025-12-31 11:16:44 -0700

    CreatedBranchKNN

--- Default_crdt_clients.py
+++ Default_crdt_clients.py
@@ -1,28 +1,41 @@
+import math
+import os
+import random
 from pathlib import Path
+from collections import defaultdict
+from itertools import product
+
 import pandas as pd
 import numpy as np
-from pandas import DataFrame 
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+
+from sklearn import preprocessing
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.linear_model import LogisticRegression , LogisticRegressionCV
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.metrics import confusion_matrix, precision_recall_curve
 from sklearn.metrics import roc_curve, accuracy_score, roc_auc_score
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score, KFold, GridSearchCV
+
+from xgboost import XGBClassifier
+from imblearn.over_sampling import SMOTE, ADASYN, BorderlineSMOTE
+from pygam import LinearGAM, s, f, l
 
 import statsmodels.api as sm
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 
-from imblearn.over_sampling import SMOTE, ADASYN, BorderlineSMOTE
-from pygam import LinearGAM, s, f, l
+from dmba import plotDecisionTree, textDecisionTree, classificationSummary
 
-
-from dmba import classificationSummary
-
-import seaborn as sns
-import matplotlib.pyplot as plt
+from pandas import DataFrame
 
 data = pd.read_excel('Data/default_crdtcard.xls',skiprows=1)
 #print(data.info())
@@ -101,13 +114,13 @@ features = ['LIMIT_BAL', 'PAY_0', 'PAY_2', 'PAY_3', 'PAY_4', 'PAY_5', 'PAY_6',
        'EDUCATION_2', 'EDUCATION_3', 'EDUCATION_4', 'MARRIAGE_2',
        'MARRIAGE_3']
 
-outcome = ['default payment next month']
+outcome = 'default payment next month'
 
 
 #split data for test/train then ensure equal probabilities for default/no default to ensure less bias in model
 
 X=data_processed[features]
-y=data_processed['default payment next month']
+y=data_processed[outcome]
 # 1. Split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.5, random_state=42, stratify=y
@@ -122,6 +135,36 @@ scaler=StandardScaler()
 X_trained_scaled = scaler.fit_transform(X_train_res)
 X_test_scaled = scaler.transform(X_test)
 
+###### We're going to do KNN here.
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_trained_scaled,y_train_res)
+
+knn_trainp= knn.predict_proba(X_trained_scaled)[:,0]
+knn_testp=knn.predict_proba(X_test_scaled)[:,1]
+
+print(knn_testp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #logistic Regression
@@ -132,8 +175,6 @@ y_pred_prob = LR.predict_proba(X_test_scaled)[:,1]
 y_pred_class = LR.predict(X_test_scaled)
 
 #confusion Matrix
-
-
 cm = confusion_matrix(y_test, y_pred_class)
 TN, FP, FN, TP = cm.ravel()
 
